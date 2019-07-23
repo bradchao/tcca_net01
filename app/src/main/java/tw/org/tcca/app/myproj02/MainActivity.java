3da4310278225cfd5b291cdadbe90d96b7f1f4e2
Brad <brad@brad.tw>
2019-07-23 21:18:54 +0800

    2019072306

--- app/src/main/java/tw/org/tcca/app/myproj02/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/myproj02/MainActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
@@ -51,7 +53,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendUDP(View view) {
-        
+        new Thread(){
+            @Override
+            public void run() {
+                String stringInput = input.getText().toString();
+                byte[] buf = stringInput.getBytes();
+
+                try {
+                    DatagramSocket socket = new DatagramSocket();
+                    DatagramPacket packet = new DatagramPacket(
+                            buf, buf.length,
+                            InetAddress.getByName("10.0.1.1"),8888
+                    );
+                    socket.send(packet);
+                    socket.close();
+                    Log.v("brad", "Send OK");
+
+                }catch (Exception e){
+                    Log.v("brad", e.toString());
+                }
+            }
+        }.start();
 
     }
+
+
 }
