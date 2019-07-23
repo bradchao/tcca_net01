19fd5583085f0171386415cbc405536569d0e7d4
Brad <brad@brad.tw>
2019-07-23 19:46:55 +0800

    init

---
+++ app/src/main/java/tw/org/tcca/app/myproj02/MainActivity.java
@@ -0,0 +1,18 @@
+package tw.org.tcca.app.myproj02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getIP(View view) {
+    }
+}
