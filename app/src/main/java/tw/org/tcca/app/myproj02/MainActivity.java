e96396c719cc9db2ae926dc7319d39f0fe3c3884
Brad <brad@brad.tw>
2019-07-23 20:31:40 +0800

    2019072303

--- app/src/main/java/tw/org/tcca/app/myproj02/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/myproj02/MainActivity.java
@@ -3,8 +3,10 @@ package tw.org.tcca.app.myproj02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 
@@ -31,10 +33,16 @@ public class MainActivity extends AppCompatActivity {
                     NetworkInterface.getNetworkInterfaces();
             while (en.hasMoreElements()){
                 NetworkInterface networkInterface = en.nextElement();
+                Enumeration<InetAddress> ips =
+                        networkInterface.getInetAddresses();
+                while (ips.hasMoreElements()){
+                    InetAddress ip = ips.nextElement();
+                    Log.v("brad", ip.getHostAddress());
+                }
 
             }
         }catch (Exception e){
-
+            Log.v("brad", e.toString());
         }
     }
 
