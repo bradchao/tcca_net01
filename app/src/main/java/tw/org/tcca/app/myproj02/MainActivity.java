656d9d6c9f5d1320dd8b2b7dd4f5d69e5cd40785
Brad <brad@brad.tw>
2019-07-23 21:00:05 +0800

    2019072305

--- app/src/main/java/tw/org/tcca/app/myproj02/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/myproj02/MainActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = findViewById(R.id.input);
     }
 
     public void getIP(View view) {
@@ -46,4 +50,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void sendUDP(View view) {
+        
+
+    }
 }
